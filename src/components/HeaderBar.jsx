88524d15956a1099b90d4b26bed3e7b4bed688ce
cjgarcia12 <christian.jgarcia03@gmail.com>
2024-07-17 10:14:20 -0400

    Added Edit and Delete buttons to the CardPosts, and Made the side bar page for my posts and Create

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -1,9 +1,36 @@
-import  MenuIcon  from '@mui/icons-material/Menu';
-import { AppBar, Box, Toolbar, Typography, Button, IconButton, InputBase} from '@mui/material';
+import * as React from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Box, Toolbar, Typography, Button, IconButton, InputBase, Drawer, List, ListItemIcon, Divider, ListItem, ListItemButton, ListItemText } from '@mui/material';
 import { styled, alpha } from '@mui/material/styles';
 import SearchIcon from '@mui/icons-material/Search';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export default function HeaderBar() {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {['My Posts', 'Create Post'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <AddCircleIcon /> : <AccountCircleIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </Box>
+  );
+
   const Search = styled('div')(({ theme }) => ({
     position: 'relative',
     borderRadius: theme.shape.borderRadius,
@@ -18,7 +45,7 @@ export default function HeaderBar() {
       width: 'auto',
     },
   }));
-  
+
   const SearchIconWrapper = styled('div')(({ theme }) => ({
     padding: theme.spacing(0, 2),
     height: '100%',
@@ -28,7 +55,7 @@ export default function HeaderBar() {
     alignItems: 'center',
     justifyContent: 'center',
   }));
-  
+
   const StyledInputBase = styled(InputBase)(({ theme }) => ({
     color: 'inherit',
     width: '100%',
@@ -45,7 +72,7 @@ export default function HeaderBar() {
       },
     },
   }));
-  
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="fixed">
@@ -56,11 +83,15 @@ export default function HeaderBar() {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={toggleDrawer(true)}
           >
             <MenuIcon />
           </IconButton>
+          <Drawer open={open} onClose={toggleDrawer(false)}>
+            {DrawerList}
+          </Drawer>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <img src="/images/Y-logo.png" alt="Logo" style={{ width: 45}} />
+            <img src="/images/Y-logo.png" alt="Logo" style={{ width: 45 }} />
           </Typography>
           <Search>
             <SearchIconWrapper>
@@ -77,3 +108,4 @@ export default function HeaderBar() {
     </Box>
   );
 }
+
